0369fda197f300dc0c0408071de361cb657ec52b
Chris Gresty <chris.gresty@gmail.com>
2019-12-14 21:57:00 +0000

    Day 11

--- src/main/kotlin/Day11SpacePolice.kt
+++ src/main/kotlin/Day11SpacePolice.kt
@@ -1,37 +1,66 @@
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import java.io.File
-import java.util.concurrent.Executors
+import kotlin.math.max
+import kotlin.math.min
 
-class Robot(val computer: IntcodeComputer09) {
+val LOG = LoggerFactory.getLogger("")
+class Robot(val computer: IntcodeComputer) {
     var loc = Point(0, 0)
     var dir = 0
+    var min = loc
+    var max = loc
 
     val dirs = arrayOf(Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0))
-    val panel = mutableSetOf<Point>()
-    val executor = Executors.newSingleThreadExecutor()
+    val panel = mutableMapOf<Point, Int>()
+    val colours = arrayOf("  ", "XX")
+
+    init {
+        panel[loc] = 1
+    }
 
     fun paint(colour: Int) {
-        panel.add(loc)
+        panel[loc] = colour
     }
 
     fun move(turn: Int) {
-        dir = (dir + 2 * turn - 1) % 4
+        dir = Math.floorMod(dir + 2 * turn - 1, 4)
         loc += dirs[dir]
+        min = Point(min(min.x, loc.x), min(min.y, loc.y))
+        max = Point(max(max.x, loc.x), max(max.y, loc.y))
     }
 
-    fun execute() =
-        executor.submit {
-            var running = true
-            while(running) {
-                val colour = computer.getOutput().take()
-                if (colour == 99L) {
-                    running = false
-                } else {
-                    paint(colour.toInt())
-                    val turn = computer.getOutput().take()
-                    move(turn.toInt())
-                }
+    fun colour() = colour(loc)
+
+    fun colour(at: Point) = panel.getOrDefault(at, 0)
+
+    fun renderPanel() {
+        for (y in min.y..max.y) {
+            for (x in min.x..max.x) {
+                print(colours[colour(Point(x, y))])
+            }
+            println()
+        }
+    }
+
+    suspend fun execute() {
+
+        var running = true
+        while(running) {
+            computer.input.send(colour().toLong())
+            try {
+                val colour = computer.output.receive()
+                paint(colour.toInt())
+                val turn = computer.output.receive()
+                move(turn.toInt())
+            } catch (ex: ClosedReceiveChannelException) {
+                running = false
             }
         }
+        LOG.info("Robot done")
+    }
 
     fun paintedPanels() = panel.size
 }
@@ -39,13 +68,19 @@ class Robot(val computer: IntcodeComputer09) {
 data class Point(val x: Int, val y:Int)
 operator fun Point.plus(other: Point) = Point(x + other.x, y + other.y)
 
-val computer = IntcodeComputer09()
+val computer = IntcodeComputer()
 
-fun main() {
+fun main() = runBlocking {
+    LOG.info("Loading program")
     computer.load(File("day11.in").readText())
+    LOG.info("Creating robot")
     val robot = Robot(computer)
-    val future = robot.execute()
+    LOG.info("Launching robot")
+    val job = launch { robot.execute() }
+    LOG.info("Executing program")
     computer.execute()
-    future.get()
-    println("${robot.paintedPanels()} panles painted.")
+    LOG.info("Waiting for robot to complete")
+    job.join()
+    println("${robot.paintedPanels()} panels painted.")
+    robot.renderPanel()
 }
---
+++ src/main/kotlin/IntcodeComputer.kt
@@ -0,0 +1,179 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
+import java.lang.RuntimeException
+import kotlin.math.pow
+
+
+class IntcodeComputer {
+
+    val LOG = LoggerFactory.getLogger("")
+
+    private val MEMSIZE = 4096
+
+    private var memory = LongArray(MEMSIZE) { 0 }
+    private var ic = 0
+    private var relativeBase = 0
+
+    private var running = false
+
+    val output = Channel<Long>(50)
+    val input = Channel<Long>(50)
+
+    fun load(program: String) {
+        program.split(",")
+                .map { it.toLong() }
+                .toLongArray()
+                .copyInto(memory)
+    }
+
+    fun set(loc: Int, value: Long) {
+        memory[loc] = value
+    }
+
+    fun get(loc: Int) : Long {
+        return memory[loc]
+    }
+
+    suspend fun execute() {
+        running = true
+        while(running) {
+            val (opcode, mode) = decodeInstruction(memory[ic])
+            ic = when (opcode) {
+                1 -> add(mode)
+                2 -> multiply(mode)
+                3 -> input(mode)
+                4 -> output(mode)
+                5 -> jumpIfTrue(mode)
+                6 -> jumpIfFalse(mode)
+                7 -> lessThan(mode)
+                8 -> eqals(mode)
+                9 -> setRel(mode)
+                99 -> halt()
+                else -> error(opcode)
+            }
+        }
+    }
+
+    fun readInputFromStdin() {
+        GlobalScope.launch {
+            while (true) {
+                input.send(readLine()!!.toLong())
+            }
+        }
+    }
+
+    fun writeOutputToStdOut() {
+        GlobalScope.launch {
+            while (true) {
+                println(output.receive())
+            }
+        }
+    }
+
+    private fun decodeInstruction(instr: Long) : Pair<Int, IntArray> {
+        val a = asArray(instr.toInt())
+        val opcode = value(a, 0) + 10 * value(a, 1)
+        val mode = intArrayOf(value(a, 2), value(a, 3), value(a, 4), value(a, 5))
+        return opcode to mode
+    }
+
+    private fun asArray(num: Int) : IntArray {
+        val a = IntArray(6)
+        for (i in 0..5) {
+            a[i] = num % (10.0.pow(i + 1).toInt()) / (10.0.pow(i).toInt())
+        }
+        return a
+    }
+
+    fun value(a: IntArray, i: Int) = if (i < a.size) a[i] else 0
+
+    fun readParam(pos: Int, mode: IntArray) : Long {
+        return when (val paramMode = mode[pos - 1]) {
+            0 -> memory[memory[ic + pos].toInt()]
+            1 -> memory[ic + pos]
+            2 -> memory[relativeBase + memory[ic + pos].toInt()]
+            else -> throw RuntimeException("Invalid mode for reading: $paramMode")
+        }
+    }
+
+    fun writeParam(pos: Int, value: Long, mode: IntArray) {
+        when (val paramMode = mode[pos - 1]) {
+            0 -> memory[memory[ic + pos].toInt()] = value
+            2 -> memory[relativeBase + memory[ic + pos].toInt()] = value
+            else -> throw RuntimeException("Invalid mode for writing: $paramMode")
+        }
+    }
+
+    fun add(mode: IntArray) : Int {
+        val a = readParam(1, mode)
+        val b = readParam(2, mode)
+        writeParam(3, a + b, mode)
+        return ic + 4
+    }
+
+    fun multiply(mode: IntArray) : Int {
+        val a = readParam(1, mode)
+        val b = readParam(2, mode)
+        writeParam(3, a * b, mode)
+        return ic + 4
+    }
+
+    suspend fun input(mode: IntArray) : Int {
+        val value = input.receive()
+        writeParam(1, value, mode)
+        return ic + 2
+    }
+
+    suspend fun output(mode: IntArray) : Int {
+        val value = readParam(1, mode)
+        println("Output: $value")
+        output.send(value)
+        return ic + 2
+    }
+
+    fun jumpIfTrue(mode: IntArray) : Int {
+        val a = readParam(1, mode)
+        val b = readParam(2, mode)
+        return if (a != 0L) b.toInt() else ic + 3
+    }
+
+    fun jumpIfFalse(mode: IntArray) : Int {
+        val a = readParam(1, mode)
+        val b = readParam(2, mode)
+        return if (a == 0L) b.toInt() else ic + 3
+    }
+
+    fun lessThan(mode: IntArray) : Int {
+        val a = readParam(1, mode)
+        val b = readParam(2, mode)
+        writeParam(3, if (a < b) 1 else 0, mode)
+        return ic + 4
+    }
+
+    fun eqals(mode: IntArray) : Int {
+        val a = readParam(1, mode)
+        val b = readParam(2, mode)
+        writeParam(3, if (a == b) 1 else 0, mode)
+        return ic + 4
+    }
+
+    fun setRel(mode: IntArray) : Int {
+        val a = readParam(1, mode).toInt()
+        relativeBase += a
+        return ic + 2
+    }
+
+    fun halt() : Int {
+        running = false
+        output.close()
+        return ic + 1
+    }
+
+    fun error(opcode: Int) : Int {
+        println("Error: Unknown opcode $opcode")
+        running = false
+        return ic + 1
+    }
+}
